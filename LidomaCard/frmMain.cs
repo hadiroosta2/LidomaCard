c8b13b4644c8b42644bc790b5a164dd7d3f0cc3c
unknown <Administrator@PERSIAGROUP.LOCAL>
2020-07-25 13:47:26 +0430

    add read excel file

--- LidomaCard/frmMain.cs
+++ LidomaCard/frmMain.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -19,6 +20,7 @@ namespace LidomaCard
         bool key = false;
         int index;
         PrintDocument pd;
+        DataTable dtExcel;
         List<string> imageFiles;
         public frmMain()
         {
@@ -359,6 +361,58 @@ namespace LidomaCard
             {
                 pd.PrinterSettings = print.PrinterSettings;
                
+            }
+        }
+
+        public DataTable ReadExcel(string fileName, string fileExt)
+        {
+            string conn = string.Empty;
+            DataTable dtexcel = new DataTable();
+            if (fileExt.CompareTo(".xls") == 0)//compare the extension of the file
+                conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';";//for below excel 2007
+            else
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1';";//for above excel 2007
+            using (OleDbConnection con = new OleDbConnection(conn))
+            {
+                try
+                {
+                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [Sheet1$]", con);//here we read data from sheet1
+                    oleAdpt.Fill(dtexcel);//fill excel data into dataTable
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            return dtexcel;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+            string fileExt = string.Empty;
+            OpenFileDialog file = new OpenFileDialog();//open dialog to choose file
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)//if there is a file choosen by the user
+            {
+                filePath = file.FileName;//get the path of the file
+                fileExt = Path.GetExtension(filePath);//get the file extension
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+
+                        dtExcel = ReadExcel(filePath, fileExt);//read excel file
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);//custom messageBox to show error
+                }
             }
         }
     }
